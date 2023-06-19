a586722e17fc7a359be387e85a4cc4c75cf210c4
Satoc4n <80965620+Satoc4n@users.noreply.github.com>
2023-06-19 17:47:03 +0200

    Added changing isOnline counter in SQL to 0 when the user logs out.

--- logout.php
+++ logout.php
@@ -1,7 +1,16 @@
 <?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'phplogin';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
     //session_start() creates a session or resumes the current one based
     // on a session identifier passed via a GET or POST request, or passed via a cookie.
     session_start();
+    // Set the isOnline in SQL Database to "0" when user is logging out.
+    $session_id = $_SESSION['id'];
+    $querySql = mysqli_query($con, "UPDATE accounts SET isOnline=0 WHERE id = '$session_id'");
     //session_destroy() destroys all data associated with the current session.
     session_destroy();
     //session_unset() destroys all variables
